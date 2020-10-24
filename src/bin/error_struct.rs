f5c5ac8ca53dcaae26f4e42a9597204fe6db48d9
ka_ueno <ka.ueno@acca-int.co.jp>
2020-10-24 12:34:28 +0900

    .

---
+++ src/bin/error_struct.rs
@@ -0,0 +1,16 @@
+struct User {
+    // this code is error because of missing lifetime specifier
+    username: &str,
+    email: &str,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn main() {
+    let user1 = User {
+        email: "someone@example.com",
+        username: "someusername123",
+        active: true,
+        sign_in_count: 1,
+    };
+}
