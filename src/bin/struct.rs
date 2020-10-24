21aa24a9356aa42c38368548c4ddd3982eb452e1
ka_ueno <ka.ueno@acca-int.co.jp>
2020-10-24 12:28:29 +0900

    .

--- src/bin/struct.rs
+++ src/bin/struct.rs
@@ -1,72 +1,66 @@
 struct User {
-    username: Name,
-    email: Email,
+    username: String,
+    email: String,
     sign_in_count: u64,
     active: bool,
 }
-struct Name {
-    first_name: String,
-    last_name: String,
-    middle_name: String,
-}
-impl Name {
-    fn get(&self) -> String {
-        format!(
-            "{} {} {}",
-            self.first_name, self.middle_name, self.last_name
-        )
-    }
-}
-
-struct Email {
-    domain: String,
-    user_part: String,
-}
-
-impl Email {
-    fn get(&self) -> String {
-        return format!("{}@{}", self.user_part, self.domain);
-    }
-}
 fn main() {
-    let mut mail = Email {
-        domain: String::from("gmail.com"),
-        user_part: String::from("k.ueno.0505"),
-    };
-    let mut name = Name {
-        first_name: String::from("kazuki"),
-        last_name: String::from("Ueno"),
-        middle_name: String::from(""),
-    };
-    let mut user = User {
-        email: mail,
-        username: name,
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
         active: true,
         sign_in_count: 1,
     };
-    println!(
-        "name is {}, email is {}, active is {},sign in count is {}",
-        user.username.get(),
-        user.email.get(),
-        user.active,
-        user.sign_in_count
-    );
-    mail = Email {
-        domain: String::from("gmail.com"),
-        user_part: String::from("ka.ueno"),
+    println!("{}", user1.username);
+
+    // this code is error because user1 is not mutable
+    // user1.email = String::from("test@example.com");
+
+    // if you want to be mutable, use mut for user variable
+    // you can't use mut for values
+    let mut user2 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
     };
-    name = Name {
-        first_name: String::from("tanaka"),
-        last_name: String::from("taro"),
-        middle_name: String::from(""),
-    };
-    user.email = mail;
-    user.username = name;
-    println!(
-        "name is {}, email is {}, active is {},sign in count is {}",
-        user.username.get(),
-        user.email.get(),
-        user.active,
-        user.sign_in_count
+    println!("{}", user2.email);
+    user2.email = String::from("anotheremail@example.com");
+    println!("{}", user2.email);
+
+    // easy to write like this
+    let build_user = build_user(
+        String::from("tanaka_taro@example.com"),
+        String::from("tanaka taro"),
     );
+    println!("{}", build_user.email);
+
+    // if you want to use other instance value , you can write like this
+    let user3 = User {
+        email: String::from(""),
+        username: String::from(""),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count,
+    };
+
+    // and like this
+    let user4 = User {
+        email: String::from(""),
+        username: String::from(""),
+        ..user1
+    };
+
+    // can I write like this ??
+    let user5 = User { ..user1 };
+    println!("{}", user5.email);
+    // this code is error because user1 is moved to user5
+    // println!("{}", user1.email);
+}
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
